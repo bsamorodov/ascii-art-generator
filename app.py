d754ff789369ea9302ec354fe264fb749945b7fb
Boris Samorodov <bsam@passap.ru>
2025-09-14 18:48:08 +0200

    feat: Use all pyfiglet fonts
    Closes #1

--- app.py
+++ app.py
@@ -1,44 +1,29 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import pyfiglet
+from pyfiglet import Figlet, FigletFont
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-# A list of some common figlet fonts
-fonts = [
-    'standard',
-    'slant',
-    '3-d',
-    '3x5',
-    '5lineoblique',
-    'alphabet',
-    'banner3-d',
-    'doh',
-    'isometric1',
-    'letters',
-    'alligator',
-    'dotmatrix',
-    'bubble',
-    'bulbhead',
-    'digital',
-    'block'
-]
+
+fonts = FigletFont.getFonts()
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     fonts.sort()
     return templates.TemplateResponse("index.html", {"request": request, "fonts": fonts, "ascii_art": ""})
 
+
 @app.post("/", response_class=HTMLResponse)
 async def generate_art(request: Request, text: str = Form(...), font: str = Form(...)):
     ascii_art = ""
     if text and font:
         try:
-            fig = pyfiglet.Figlet(font=font)
+            fig = Figlet(font=font)
             ascii_art = fig.renderText(text)
-        except pyfiglet.FontNotFound:
+        except FontNotFound:
             ascii_art = "Font not found!"
     return templates.TemplateResponse("index.html", {"request": request, "fonts": fonts, "ascii_art": ascii_art})
