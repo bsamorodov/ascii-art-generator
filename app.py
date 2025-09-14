52062a3cc99d14d21eec9bbdd00ec5c991aa6c2d
Boris Samorodov <bsamorodov@users.noreply.github.com>
2025-09-14 15:08:02 +0200

    Add project files

---
+++ app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pyfiglet
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+# A list of some common figlet fonts
+fonts = [
+    'standard',
+    'slant',
+    '3-d',
+    '3x5',
+    '5lineoblique',
+    'alphabet',
+    'banner3-d',
+    'doh',
+    'isometric1',
+    'letters',
+    'alligator',
+    'dotmatrix',
+    'bubble',
+    'bulbhead',
+    'digital'
+]
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "fonts": fonts, "ascii_art": ""})
+
+@app.post("/", response_class=HTMLResponse)
+async def generate_art(request: Request, text: str = Form(...), font: str = Form(...)):
+    ascii_art = ""
+    if text and font:
+        try:
+            fig = pyfiglet.Figlet(font=font)
+            ascii_art = fig.renderText(text)
+        except pyfiglet.FontNotFound:
+            ascii_art = "Font not found!"
+    return templates.TemplateResponse("index.html", {"request": request, "fonts": fonts, "ascii_art": ascii_art})
